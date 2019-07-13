a462c1115cec2ea00ba4ebcbe44519d709a7f425
Fethi Isfarca <fisfarca@googlemail.com>
2019-07-13 08:32:49 +0200

    Standard text write to input field.

---
+++ Carrot_Position_VR/Assets/Scripts/UI/CarrotCanvas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarrotCanvas : MonoBehaviour
+{
+    [SerializeField] private InputField carrotInputField;
+    [SerializeField] private Button setCarrotPositionButton;
+
+    private void Awake()
+    {
+        carrotInputField.text = "Hello World";
+    }
+}
