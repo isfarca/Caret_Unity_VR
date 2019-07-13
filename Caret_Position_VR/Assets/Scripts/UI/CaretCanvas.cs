3e3d88aef9ed35ce7e41099dcfeed08c8274d191
Fethi Isfarca <fisfarca@googlemail.com>
2019-07-13 10:34:58 +0200

    Change caret position by random number.

---
+++ Caret_Position_VR/Assets/Scripts/UI/CaretCanvas.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CaretCanvas : MonoBehaviour
+{
+    // UI elements.
+    [SerializeField] private InputField caretInputField;
+    [SerializeField] private Button setCaretPositionButton;
+    [SerializeField] private Text caretPositionText;
+
+    // Caret members.
+    private int caretPosition;
+    
+    /// <summary>
+    /// Input field click per code.
+    /// </summary>
+    private void ClickInputField()
+    {
+        EventSystem current;
+        (current = EventSystem.current).SetSelectedGameObject(caretInputField.gameObject, null);
+        caretInputField.OnPointerClick(new PointerEventData(current));
+    }
+
+    /// <summary>
+    /// Default initializing.
+    /// </summary>
+    private void Awake()
+    {
+        // Set input field text.
+        caretInputField.text = "Hello World";
+    }
+
+    /// <summary>
+    /// Action initializing.
+    /// </summary>
+    private void Start()
+    {
+        // Add listeners.
+        setCaretPositionButton.onClick.AddListener(OnSetCaretPositionClick);
+        
+        // Input field click.
+        ClickInputField();
+    }
+
+    /// <summary>
+    /// Change UI element values.
+    /// </summary>
+    private void LateUpdate()
+    {
+        // Change caret position.
+        caretInputField.caretPosition = caretPosition;
+    }
+
+    /// <summary>
+    /// Set random caret position.
+    /// </summary>
+    private void OnSetCaretPositionClick()
+    {
+        // Input field click.
+        ClickInputField();
+        
+        // Change caret position from random number.
+        var randomCaretPosition = Random.Range(0, caretInputField.text.Length);
+        caretPositionText.text = randomCaretPosition.ToString();
+        caretPosition = randomCaretPosition;
+    }
+}
